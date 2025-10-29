cdd50977be0e7824efdd05ef5aa6d92c3678d05f
Eliezer-Carvalho <eliezer.carvalho.11.8a@gmail.com>
2025-10-29 19:26:54 +0000

    main

--- snakegame.c
+++ snakegame.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
-
+#include <conio.h>
+#include <stdlib.h>
 
 int main() {
 
 const int altura = 11;
 const int largura = 21;
-const int posicao_cobra_altura = altura / 2;
-const int posicao_cobra_largura = largura / 2;
+int posicao_cobra_altura = altura / 2;
+int posicao_cobra_largura = largura / 2;
+char tecla;
+
+while (1) {
+    system("cls");
+    for (int i = 0; i < altura; i++) {
+        for (int j = 0; j < largura; j ++) {
+
+            if (i == 0 || i == altura - 1)
+                printf(" # ");
+
+            else if (i == posicao_cobra_altura && j == posicao_cobra_largura)
+                printf(" o ");
 
 
-for (int i = 0; i < altura; i++) {
-    for (int j = 0; j < largura; j ++) {
+            else if (j == 0 || j == largura - 1)
+                printf(" # ");
 
-        if (i == 0 || i == altura - 1)
-            printf(" # ");
-
-        else if (i == posicao_cobra_altura && j == posicao_cobra_largura)
-            printf(" o ");
-
-
-        else if (j == 0 || j == largura - 1)
-            printf(" # ");
-
-        else    
-            printf("   ");
+            else    
+                printf("   ");
+        }
+        printf ("\n");
     }
-    printf ("\n");
+
+    printf("Use W, A, S, D para mover a cobra!");
+    tecla = getch();
+
+    if (tecla == 'w') posicao_cobra_altura++;
+    if (tecla == 's') posicao_cobra_altura--;
+    if (tecla == 'a') posicao_cobra_largura--;
+    if (tecla == 'd') posicao_cobra_largura++;
+    if (tecla == 'b') break;
 }
+
 
     return 0;
 }

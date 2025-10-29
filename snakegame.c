f8d9ae72062ef671b740a970add1863a0651d70c
Eliezer-Carvalho <eliezer.carvalho.11.8a@gmail.com>
2025-10-29 18:57:39 +0000

    main

--- snakegame.c
+++ snakegame.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 
+
 int main() {
 
-    printf("Hello World");
+const int altura = 11;
+const int largura = 21;
+const int posicao_cobra_altura = altura / 2;
+const int posicao_cobra_largura = largura / 2;
+
+
+for (int i = 0; i < altura; i++) {
+    for (int j = 0; j < largura; j ++) {
+
+        if (i == 0 || i == altura - 1)
+            printf(" # ");
+
+        else if (i == posicao_cobra_altura && j == posicao_cobra_largura)
+            printf(" o ");
+
+
+        else if (j == 0 || j == largura - 1)
+            printf(" # ");
+
+        else    
+            printf("   ");
+    }
+    printf ("\n");
+}
 
     return 0;
 }

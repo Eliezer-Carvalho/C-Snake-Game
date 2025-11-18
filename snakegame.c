e4391e3ae5a835b3ec64acecd9a5266101e97828
Eliezer-Carvalho <eliezer.carvalho.11.8a@gmail.com>
2025-11-18 19:29:05 +0000

    main

--- snakegame.c
+++ snakegame.c
@@ -8,6 +8,8 @@ const int altura = 11;
 const int largura = 21;
 int posicao_cobra_altura = altura / 2;
 int posicao_cobra_largura = largura / 2;
+int food_x = rand() % (largura - 1) + 1;
+int food_y = rand() % (altura - 1) + 1;
 char tecla;
 
 while (1) {
@@ -15,18 +17,18 @@ while (1) {
     system("cls"); /* clear, limpa para receber um novo input todas as vezes*/
     printf("Use W, A, S, D para mover a cobra! \n");
 
+
     for (int i = 0; i < altura; i++) {
         for (int j = 0; j < largura; j ++) {
 
-            if (i == 0 || i == altura - 1) 
+            if (i == 0 || i == altura - 1 || j == 0 || j == largura - 1) 
                 printf(" # "); /* EIXO DAS LINHAS */
-            
+
             else if (i == posicao_cobra_altura && j == posicao_cobra_largura) 
                 printf(" o "); /* POSIÇÃO INICIAL DA COBRA QUE É O CENTRO DO TABULEIRO */
             
-
-            else if (j == 0 || j == largura - 1) 
-                printf(" # "); /* EIXO DAS COLUNAS */
+            else if (i == food_x && j == food_y)
+                printf(" . ");
             
             else 
                 printf("   "); /* VAZIO DENTRO DA TABELA  */
@@ -44,10 +46,18 @@ while (1) {
     if (tecla == 'd') posicao_cobra_largura++;
     if (tecla == 'b') break;
 
+
+    
+
+
     if (posicao_cobra_altura == 0 || posicao_cobra_altura == altura - 1 || posicao_cobra_largura == 0 || posicao_cobra_largura == largura - 1) {
         printf("Terminou o jogo! :)");
         break; 
     }
+
+
+
+
 
 }
 

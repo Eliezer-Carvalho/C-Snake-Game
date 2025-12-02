0d3ecf4bcc277b573468254ef7320cb905164ce8
Eliezer-Carvalho <eliezer.carvalho.11.8a@gmail.com>
2025-12-02 18:08:17 +0000

    Main

--- snakegame.c
+++ snakegame.c
@@ -8,28 +8,31 @@ const int altura = 11;
 const int largura = 21;
 int posicao_cobra_altura = altura / 2;
 int posicao_cobra_largura = largura / 2;
-int food_x = rand() % (largura - 1) + 1;
-int food_y = rand() % (altura - 1) + 1;
+int food_x = rand() % (largura + 1);
+int food_y = rand() % (altura + 1);
 int score;
 char tecla;
+
 
 while (1) {
 
     system("cls"); /* clear, limpa para receber um novo input todas as vezes*/
     printf("Use W, A, S, D para mover a cobra! \n");
+    printf("\n");
 
-
-    for (int i = 0; i < altura; i++) {
-        for (int j = 0; j < largura; j ++) {
+    for (int i = 0; i < altura; i++) 
+    {
+        for (int j = 0; j < largura; j ++) 
+        {
 
             if (i == 0 || i == altura - 1 || j == 0 || j == largura - 1) 
-                printf(" # "); /* EIXO DAS LINHAS */
+                printf(" # "); /* TABELA DE JOGO no eixo dos X e Y */
 
             else if (i == posicao_cobra_altura && j == posicao_cobra_largura) 
                 printf(" o "); /* POSIÇÃO INICIAL DA COBRA QUE É O CENTRO DO TABULEIRO */
             
             else if (i == food_x && j == food_y)
-                printf(" . ");
+                printf(" . "); /* posição aleatória da comida */
             
             else 
                 printf("   "); /* VAZIO DENTRO DA TABELA  */
@@ -38,7 +41,8 @@ while (1) {
         printf ("\n");
     }
 
-    
+    printf("SCORE: %d\n", score);
+
     tecla = getch();
 
     if (tecla == 'w') posicao_cobra_altura--;
@@ -47,24 +51,21 @@ while (1) {
     if (tecla == 'd') posicao_cobra_largura++;
     if (tecla == 'b') break;
 
-/* VERIFICAR ESTE CÓDIGO E CONTINUAR - 18/11*/
+/*ERRO NA COMIDA
+ERRO NO SCORE*/
 
-    if (posicao_cobra_altura == food_x && posicao_cobra_largura == food_y) {
+    if (posicao_cobra_altura == food_x && posicao_cobra_largura == food_y) 
+    {
+        food_x = rand() % (largura + 1);
+        food_y = rand() % (altura + 1);
         score = score + 10;
-        food_x = rand() % (largura - 1) + 1;
-        food_y = rand() % (altura - 1) + 1;
-        printf("Pontuação : %d", score);
     }
 
-
-    if (posicao_cobra_altura == 0 || posicao_cobra_altura == altura - 1 || posicao_cobra_largura == 0 || posicao_cobra_largura == largura - 1) {
+    if (posicao_cobra_altura == 0 || posicao_cobra_altura == altura - 1 || posicao_cobra_largura == 0 || posicao_cobra_largura == largura - 1) 
+    {
         printf("Terminou o jogo! :)");
         break; 
     }
-
-
-
-
 
 }
 
